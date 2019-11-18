46161f4cfd8f73f7a53b9189e0a753b17cafba74
pierremichiels <57896327+pierremichiels@users.noreply.github.com>
2019-11-18 11:35:02 +0100

    Update Db.class.php

--- models/Db.class.php
+++ models/Db.class.php
@@ -6,18 +6,16 @@ class Db
 
     private function __construct()
     {
-        try {
-            $this->_db = new PDO('mysql:host=localhost;dbname=bdbn;charset=utf8', 'root', '');
-            $this->_db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-			$this->_db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
-        } 
-		catch (PDOException $e) {
-		    die('Erreur de connexion à la base de données : '.$e->getMessage());
-        }
+       	$db = parse_url(getenv("DATABASE_URL"));
 
-		catch (PDOException $e) {
-		    die('Erreur de connexion à la base de données : '.$e->getMessage());
-        }
+	$pdo = new PDO("pgsql:" . sprintf(
+	    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+	    $db["host"],
+	    $db["port"],
+	    $db["user"],
+	    $db["pass"],
+	    ltrim($db["path"], "/")
+	));
     }
 
 	# Pattern Singleton
